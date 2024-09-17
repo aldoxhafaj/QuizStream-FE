70adbd9deb95e21a2ebf775be0f7fafb54d65b37
Aldo <xhafajaldo16@gmail.com>
2024-09-17 19:32:30 +0200

    feature/quiz-07 remove react-lottie library and solve unsafe react lifecycles warning

--- src/components/Lottie/Lottie.tsx
+++ src/components/Lottie/Lottie.tsx
@@ -1,34 +1,27 @@
 import { LottieAnimations } from '@quiz-stream/themes/animations';
-import LottieComponent from 'react-lottie';
+import LottieComponent from 'react-lottie-player';
 
 import { LottieComponentProps } from './types';
 
-//TODO test all this props
 const FULL_SCREEN = '100%';
 const PRESERVE_RATIO = 'xMidYMid slice';
 export const Lottie = ({
   animation,
   loop = false,
-  autoplay = true,
+  play = true,
   size = FULL_SCREEN,
-  isStopped = false,
-  isPaused = false,
-  isClickToPauseDisabled = false,
 }: LottieComponentProps) => {
   return (
     <LottieComponent
-      height={size}
-      width={size}
-      isStopped={isStopped}
-      isPaused={isPaused}
-      isClickToPauseDisabled={isClickToPauseDisabled}
-      options={{
-        loop,
-        autoplay,
-        animationData: LottieAnimations[animation],
-        rendererSettings: {
-          preserveAspectRatio: PRESERVE_RATIO,
-        },
+      loop={loop}
+      animationData={LottieAnimations[animation]}
+      play={play}
+      rendererSettings={{
+        preserveAspectRatio: PRESERVE_RATIO,
+      }}
+      style={{
+        width: size,
+        height: size,
       }}
     />
   );
--- src/components/Lottie/types.ts
+++ src/components/Lottie/types.ts
@@ -1,12 +1,8 @@
 import { Animation } from '@quiz-stream/themes/animations';
-import { LottieProps } from 'react-lottie';
 
-export type LottieComponentProps = Pick<
-  LottieProps,
-  'isStopped' | 'isPaused' | 'isClickToPauseDisabled'
-> & {
-  loop?: number | boolean;
-  autoplay?: boolean;
+export type LottieComponentProps = {
+  loop?: boolean;
+  play?: boolean;
   animation: Animation;
   size?: string | number;
 };
